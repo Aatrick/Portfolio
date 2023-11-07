2a6134b384bcc1f9c140ec8f85213a9496283c19
Aatrick <113598245+Aatrick@users.noreply.github.com>
2023-11-07 19:21:08 +0100

    Update base path in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://Aatrick.github.io',
-    base: '/portfolio',
+    base: '/Portfolio',
 });
